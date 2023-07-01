9af8ea756eaae1a71b2a10028751e0dedd5f0762
Pranav Reddy Lakkadi <95748382+PranavLakkadi13@users.noreply.github.com>
2023-07-02 01:58:53 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
-// require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-deploy");
